665ccc1890d3af9b793063bd921839e29aa78898
Tushar Gawande <tgawande04@gmail.com>
2024-01-11 14:46:16 +0530

    Adding address microservice

---
+++ address-app/src/main/java/com/seleniumexpress/AddressAppApplication.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressAppApplication.class, args);
+	}
+
+}
---
+++ address-app/src/test/java/com/seleniumexpress/AddressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seleniumexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
